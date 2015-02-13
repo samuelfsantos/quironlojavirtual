28dfad1d8281e0d1cb2ca73353c5a591f14089b8
samuelfsantos <samu_192.sa@hotmail.com>
2015-02-13 19:55:33 -0200

    ss

---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        //
+        // GET: /Produtos/
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+
+            return View(produtos);
+        }
+	}
+}
