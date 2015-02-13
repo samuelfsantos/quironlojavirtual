19ae6304f106cc6b513d3f50001c0bab8c56973a
samuelfsantos <samu_192.sa@hotmail.com>
2015-02-13 19:02:54 -0200

    ss

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext: DbContext
+    {
+        //public DbSet<Produto> Produtos { get; set; }
+    }
+}
